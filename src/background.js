2287f540e52f01b1818411b41c8c74c0c4ce98cf
Webnerd <stanny@mangamis.com>
2021-01-16 12:05:17 +0800

    basic layout run

--- src/background.js
+++ src/background.js
@@ -13,8 +13,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1180,
+    height: 760,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
